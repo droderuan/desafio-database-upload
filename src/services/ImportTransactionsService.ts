e481a0c8847b7c07495610e2f7642d7143982120
Ruan Ferreira <droderuan@hotmail.com>
2020-04-20 03:44:51 -0300

    Fixed the persist of transactions in ImportTransactionService.ts

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import parse from 'csv-parse/lib/sync';
-import Transaction from '../models/Transaction';
+import { getRepository, getCustomRepository, In } from 'typeorm';
 
-import CreateTransactionService from './CreateTransactionService';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 interface RequestDTO {
   title: string;
@@ -15,28 +18,64 @@ class ImportTransactionsService {
   async execute(path: string): Promise<Transaction[]> {
     const file = fs.readFileSync(path);
     const transactions: RequestDTO[] = await parse(file, {
-      columns: ['title', 'type', 'value', 'category'],
+      columns: true,
       delimiter: ', ',
+      skip_lines_with_empty_values: true,
     });
-    transactions.splice(0, 1);
 
-    const inputedTransactions: Transaction[] = [];
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoriesRepository = getRepository(Category);
 
-    const createTransaction = new CreateTransactionService();
-    // const inputedTransactions = await Promise.all(
-    //   transactions.map(async transaction => {
-    //     const createdTransaction = await createTransaction.execute(transaction);
+    // Verifying the categories to create or get
+    const categoriesFromInputs = transactions.map(
+      transaction => transaction.category,
+    );
+    const existentCategories = await categoriesRepository.find({
+      where: { title: In(categoriesFromInputs) },
+    });
+    const existentCategoriesTitle = existentCategories.map(
+      category => category.title,
+    );
 
-    //     return createdTransaction;
-    //   }),
-    // );
+    const addCategories = categoriesFromInputs
+      .filter(category => !existentCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
 
-    for (const transaction of transactions) {
-      const createdTransaction = await createTransaction.execute(transaction);
-      inputedTransactions.push(createdTransaction);
-    }
+    const newCategories = categoriesRepository.create(
+      addCategories.map(title => ({ title })),
+    );
+
+    await categoriesRepository.save(newCategories);
+
+    const allCategories = [...existentCategories, ...newCategories];
+
+    // Persisting the transactions
+    const inputedTransactions = transactionRepository.create(
+      transactions.map(({ category, ...transaction }: RequestDTO) => ({
+        ...transaction,
+        category_id: this.returnCategoryId(
+          allCategories.find(({ title }) => title === category),
+        ),
+      })),
+    );
+
+    await transactionRepository.save(inputedTransactions);
+
+    // This method has a very high CPU cost and time of execution
+    // for (const transaction of transactions) {
+    //   // eslint-disable-next-line no-await-in-loop
+    //   const createdTransaction = await createTransaction.execute(transaction);
+    //   inputedTransactions.push(createdTransaction);
+    // }
+
+    await fs.promises.unlink(path);
 
     return inputedTransactions;
+  }
+
+  // TODO: Find a way to remove this warning
+  private returnCategoryId({ id }: any): string {
+    return id;
   }
 }
 
